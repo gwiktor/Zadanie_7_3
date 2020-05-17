e6797cde2f7d8e88d4476098fb2b196f738073d4
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-05-17 18:02:32 +0200

    Add branch with code

--- Zadanie_7_3.py
+++ Zadanie_7_3.py
@@ -1,18 +1,22 @@
+import sys
 from faker import Faker
 fake_data = Faker()
 
 class BaseContact:
-    def __init__(self, name, last_name, number, e_mail):
+    def __init__(self, name, last_name, phone_number, e_mail):
         self.name = name
         self.last_name = last_name
-        self.number = number
+        self.phone_number = phone_number
         self.e_mail = e_mail
 
     def __str__(self):
-        return f"{self.name} {self.last_name} {self.e_mail}"
+        return f"{self.name} {self.last_name} {self.phone_number} {self.e_mail}"
 
     def contact(self):
-        return f"Wybieram numer +48 {self.number} i dzwonię do {self.name} {self.last_name}"
+        for self in base_cards:
+            return f"Wybieram numer +48 {self.phone_number} i dzwonię do {self.name} {self.last_name}"
+        for self in business_cards:
+            return f"Wybieram numer +48 {self.business_number} i dzwonię do {self.name} {self.last_name}"
     
     @property
     def label_length(self):
@@ -21,44 +25,38 @@ class BaseContact:
         return f"{imie} {nazwisko}"
 
 class BusinessContact(BaseContact):
-   def __init__(self, job, company, business_phone, *args, **kwargs):
-       super().__init__(*args, **kwargs)
-       self.job = job
-       self.company = company
-       self.business_phone = business_phone
+    def __init__(self, job, company, business_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.job = job
+        self.company = company
+        self.business_phone = business_phone
 
-base_cards = []
-business_cards = []
+    base_cards = []
+    business_cards = []
 
-for m in range(5):
-    i = fake_data.name()
-    j = fake_data.safe_email()
-    k = i.split()
-    m = fake_data.phone_number
-    n = fake_data.job
-    p = fake_data.company
-    r = fake_data.phone_number
+    def create_contacts(quantity, choice):
+        base_cards = []
+        business_cards = []
+        for a in range(quantity):
+            i = fake_data.name()
+            j = fake_data.safe_email()
+            k = i.split()
+            m = fake_data.msisdn()
+            n = fake_data.job()
+            p = fake_data.company()
+            r = fake_data.msisdn()
+            if choice == 1:
+                base_cards.append(BaseContact(k[0], k[1], m, j))
+                print(base_cards)
+            elif choice == 2:
+                business_cards.append(BusinessContact(k[0], k[1], m, j, n, p, r))
+                print(business_cards)
+quantity = int(input("Ile chcesz wizytówek?"))
+choice = int(input("Jakie chcesz wizytówki?"))
 
-    base_cards.append(BaseContact(k[0], k[1], m, j))
-    business_cards.append(BusinessContact(k[0], k[1], m, j, n, p, r))
 
 '''
-by_name = sorted(base_cards, key=lambda BaseContact: BaseContact.name)
-by_last_name = sorted(base_cards, key=lambda BaseContact: BaseContact.last_name)
-by_e_mail = sorted(base_cards, key=lambda BaseContact: BaseContact.e_mail)
-
-print("Cards sorted by name:")
-for i in by_name:
-    print(i)
-
-print("Cards sorted by last name:")
-for i in by_last_name:
-    print(i)
-
-print("Cards sorted by e-mail:")
-for i in by_e_mail:
-    print(i)
-
-print(base_cards[1].contact())
-print(base_cards[1].label_length)
+base_cards.append(BaseContact(k[0], k[1], m, j))
+business_cards.append(BusinessContact(k[0], k[1], m, j, n, p, r))
 '''
+
