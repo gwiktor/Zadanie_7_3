b146ad3ef45b423ee30a95ac74ce2e278217ceba
Wiktor Gabrys <gabrys.wiktor@gmail.com>
2020-05-23 20:23:06 +0200

    Add if __name__ == __main__

--- Zadanie_7_3.py
+++ Zadanie_7_3.py
@@ -1,6 +1,9 @@
+import logging
 import sys
 from faker import Faker
 fake_data = Faker()
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 class BaseContact:
     def __init__(self, name, last_name, phone_number, e_mail):
@@ -42,14 +45,23 @@ def create_contacts(choice, quantity):
         m = fake_data.msisdn()
         if choice == "BaseContact":
             base_cards.append(BaseContact(name = k[0], last_name = k[1], phone_number = m, e_mail = j))
+            for i in base_cards:
+                return(f"Lista kontaktów: {i}")
         elif choice == "BusinessContact":
             n = fake_data.job()
             p = fake_data.company()
             r = fake_data.phone_number()
             business_cards.append(BusinessContact(name = k[0], last_name = k[1], phone_number = m, e_mail = j, job = n, company = p, business_number = r))
-
+            return(f"Lista kontaktów: {business_cards}")
 
 #Sprawdzenie
+if __name__ == "__main__":
+    print("*** WITAJ W KSIĄŻCE ADRESOWEJ ***")
+    choice = input("Napisz jaką książkę adresową chcesz stworzyć, posługjąc się odpowiednim określeniem: \n1 - BaseContact \n2 - BusinessContact")
+    quantity = int(input("Podaj liczbę kontaktów, które chcesz utworzyć: "))
+    logging.debug(f"Została wybrana książka adresowa {choice} z {quantity} kontaktami")
+    logging.debug(create_contacts(choice, quantity))
+'''
 create_contacts("BaseContact", 10)
 for i in base_cards:
     print(i)
@@ -63,3 +75,4 @@ print(base_cards[5].label_length)
 
 print(business_cards[1].contact())
 print(business_cards[1].label_length)
+'''
